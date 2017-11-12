e2a2707c6d55ce92e94f1d0326328ea43e42221a
Whitney Norrad <wnorrad@gmail.com>
2017-11-12 23:13:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ let randomNumber = Math.random () * 20
 
 let randomInteger = (Math.floor(randomNumber))
 
-let randomUserID = randomInteger +100000000
+let randomUserID = randomInteger +1000000000
