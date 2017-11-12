5ad7c8419579057c3695a16de8a980bd03133cdc
Whitney Norrad <wnorrad@gmail.com>
2017-11-12 23:01:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,4 +15,4 @@ let randomNumber = Math.random () * 20
 
 let randomInteger = (Math.floor(randomNumber))
 
-let randomUserID = randomInteger(Math.random (10000000010) * 1000000020)
+let randomUserID = randomInteger(Math.random (10000000010) * 1000000021)
